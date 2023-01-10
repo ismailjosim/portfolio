b58e3006d07a57cbae86c0392aac88a8ff45149f
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-01-10 22:01:32 +0600

    add assets files and add custom css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import About from './Components/About/About';
+import Experiences from './Components/Experiences/Experiences';
+import Header from './Components/Header/Header';
+import Nav from './Components/Nav/Nav';
+import Portfolio from './Components/Portfolio/Portfolio';
+import Services from './Components/Services/Services';
+import Testimonials from './Components/Testimonials/Testimonials';
+import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div>
-      <h3>Welcome to react</h3>
+      <Header />
+      <Nav />
+      <About />
+      <Experiences />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
 }
