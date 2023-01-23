a89d2275042af519320ecc17d7b5b9bfb44a6e00
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-01-23 23:39:48 +0600

    add services section, styling and make fully responsive

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,9 +1,75 @@
 import React from 'react';
+import { BiCheck } from 'react-icons/bi';
+import "../Styles/services.css";
 
 const Services = () => {
+    const serviceItems = [
+        {
+            title: "UI/UX Design",
+            points: [
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+            ]
+        },
+        {
+            title: "Web Development",
+            points: [
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+            ]
+        },
+        {
+            title: "Content Creation",
+            points: [
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+            ]
+        },
+    ]
+
     return (
         <div id='services' className="section_wrapper">
-            <h3>services Section</h3>
+            <h5>What I Offer</h5>
+            <h2>My Services</h2>
+            <div className="container services_container">
+                {serviceItems.map((item, idx) => <div key={idx} className="service">
+                    <div className="service_heading">
+                        <h3>{item.title}</h3>
+                    </div>
+                    <ul className="service_list">
+                        {item.points.map((point, idx) => <li key={idx}>
+                            <BiCheck className='service_list_icon' />
+                            <p className="list_details">{point}</p>
+                        </li>
+                        )}
+                    </ul>
+                </div>
+                )}
+            </div>
         </div>
     );
 };
