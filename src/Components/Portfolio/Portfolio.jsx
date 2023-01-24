88e1997709abd6b6ca3dc9b3876fea3c615caa04
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-01-24 20:52:12 +0600

    add portfolio section and make styling, responsive

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import '../Styles/Portfolio.css'
+import img01 from '../../assets/portfolio1.jpg';
 
 const Portfolio = () => {
     return (
-        <div>
-
-        </div>
+        <section id='portfolio' className='section_wrapper'>
+            <h5>My Recent Work</h5>
+            <h2>Portfolio</h2>
+            <div className="container portfolio_container">
+                <article className="portfolio_item">
+                    <div className="portfolio_item_img">
+                        <img src={img01} alt="" />
+                    </div>
+                    <h3>This is a portfolio Item title</h3>
+                    <div className="cta">
+                        <a href="https://github.com" target={'_blank'} className="btn" rel="noreferrer">Github</a>
+                        <a href="https://github.com" target={'_blank'} className="btn btn-primary" rel="noreferrer">Live Demo</a>
+                    </div>
+                </article>
+            </div>
+        </section>
     );
 };
 
