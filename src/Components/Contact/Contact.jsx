9cc08c8bff54eaaf9d74f9a063682cd0a4d827fc
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-01-24 22:03:01 +0600

    add contact form and make responsive

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import { MdOutlineEmail } from 'react-icons/md'
+import { RiMessengerFill } from 'react-icons/ri'
+import { BsWhatsapp } from 'react-icons/bs';
+import '../Styles/Contact.css'
 
 const Contact = () => {
     return (
-        <div id='contact' className='section_wrapper'>
-            <h3>Contact US section</h3>
-        </div>
+        <section id='contact' className='section_wrapper'>
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+            <div className="container contact_container">
+                <div className="contact_options">
+                    <article className="contact_option">
+                        <MdOutlineEmail className='contact_option_icon' />
+                        <h5>ismailjosim@yahoo.com</h5>
+                        <a target={'_blank'} href="mailto:ismailjosim@yahoo.com" rel="noreferrer">Send a message</a>
+                    </article>
+                    <article className="contact_option">
+                        <RiMessengerFill className='contact_option_icon' />
+                        <h5>Messenger</h5>
+                        <a target={'_blank'} href="https://m.me/ismailjosim99" rel="noreferrer">Send a message</a>
+                    </article>
+                    <article className="contact_option">
+                        <BsWhatsapp className='contact_option_icon' />
+                        <h5>whatsapp</h5>
+                        <a target={'_blank'} href="https://api.whatsapp.com/send?phone+8801715052808" rel="noreferrer">Send a message</a>
+                    </article>
+                </div>
+                <form action="">
+                    <input type="text" name='name' placeholder='Your Full Name' required />
+                    <input type="email" name='email' placeholder='Your Email' required />
+                    <textarea name='message' placeholder='Your Message' rows="7" required></textarea>
+                    <button type='submit' className='btn btn-primary'>Send Message</button>
+                </form>
+
+            </div>
+        </section>
     );
 };
 
