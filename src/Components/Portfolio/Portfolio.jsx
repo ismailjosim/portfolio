3600edb4d85b8e740dfffc8537d14b82387e4563
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-01-25 23:19:33 +0600

    add dynamic projects

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Styles/Portfolio.css'
-import img01 from '../../assets/portfolio1.jpg';
+import { useEffect } from 'react';
 
 const Portfolio = () => {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        fetch('projects.json')
+            .then(res => res.json())
+            .then(data => setProjects(data))
+    }, [])
+
+
+
     return (
         <section id='portfolio' className='section_wrapper'>
             <h5>My Recent Work</h5>
             <h2>Portfolio</h2>
             <div className="container portfolio_container">
-                <article className="portfolio_item">
-                    <div className="portfolio_item_img">
-                        <img src={img01} alt="" />
-                    </div>
-                    <h3>This is a portfolio Item title</h3>
-                    <div className="cta">
-                        <a href="https://github.com" target={'_blank'} className="btn" rel="noreferrer">Github</a>
-                        <a href="https://github.com" target={'_blank'} className="btn btn-primary" rel="noreferrer">Live Demo</a>
-                    </div>
-                </article>
+                {projects.map((item, idx) => {
+                    const { name, picture, live, github, tech } = item;
+                    return <article key={idx} className="portfolio_item">
+                        <div className="portfolio_item_img">
+                            <img src={picture} alt="" />
+                        </div>
+                        <div className="portfolio_tech">
+                            {
+                                tech.map((item, idx) => <button className="btn-outline" key={idx}>{item}</button>)
+                            }
+                        </div>
+                        <h3 className='portfolio_title'>{name}</h3>
+
+                        <div className="cta">
+                            <a href={github} target={'_blank'} className="btn" rel="noreferrer">Github</a>
+                            <a href={live} target={'_blank'} className="btn btn-primary" rel="noreferrer">Livesite</a>
+                        </div>
+                    </article>
+                })}
             </div>
         </section>
     );
